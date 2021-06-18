21df41b3da3d1c45e6429b68ddc2bdc32b308934
dryabchenko <dizimka2255@mail.ru>
2021-06-18 15:49:52 +0300

    Implementer baci Pagination and added Utest

--- SwimmingStore/Controllers/HomeController.cs
+++ SwimmingStore/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SwimmingStore.Models.Repository;
 using SwimmingStore.Models;
+using System.Linq;
 
 namespace SwimmingStore.Controllers
 {
@@ -8,14 +9,19 @@ namespace SwimmingStore.Controllers
     {
         private IStoreRepository _repository;
 
+        public int PageSize = 4;
+
         public HomeController(IStoreRepository repository)
         {
             _repository = repository;
         }
 
-        public IActionResult Index()
+        public ViewResult Index(int productPage = 1)
         {
-            return View(_repository.Products);
+            return View(_repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
