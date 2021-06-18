15fbc05aa98e3500b1ef389b3fcb907f8f18abfa
dryabchenko <dizimka2255@mail.ru>
2021-06-18 16:39:05 +0300

    Added unit test for Pagination and page tag helper

---
+++ SwimmingStore.Tests/PageLinkTagHelperTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using SwimmingStore.Infrastructure;
+using SwimmingStore.Models.VIewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SwimmingStore.Tests
+{
+    public class PageLinkTagHelperTest
+    {
+        [Fact]
+        public void Can_Generate_Page_Link()
+        {
+            var urlHelper = new Mock<IUrlHelper>();
+            
+            urlHelper.SetupSequence(x =>
+            x.Action(It.IsAny<UrlActionContext>()))
+                .Returns("Test/Page1")
+                .Returns("Test/Page2")
+                .Returns("Test/Page3");
+
+            var urlHeplerFactory = new Mock<IUrlHelperFactory>();
+
+            urlHeplerFactory.Setup(f =>
+            f.GetUrlHelper(It.IsAny<ActionContext>()))
+                .Returns(urlHelper.Object);
+
+            PageLinkTagHelper helper = new PageLinkTagHelper(urlHeplerFactory.Object)
+            {
+                PageModel = new PagingInfo
+                {
+                    CurrentPage = 2,
+                    TotalItem = 28,
+                    ItemsPerPage = 10
+                },
+                PageAction = "Test"
+            };
+
+            TagHelperContext ctx = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), "");
+
+            var content = new Mock<TagHelperContent>();
+
+            TagHelperOutput output = new TagHelperOutput("div",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            helper.Process(ctx, output);
+
+            Assert.Equal(@"<a href=""Test/Page1"">1</a>"
+                + @"<a href=""Test/Page2"">2</a>"
+                + @"<a href=""Test/Page3"">3</a>",
+                output.Content.GetContent());
+        }
+    }
+}
